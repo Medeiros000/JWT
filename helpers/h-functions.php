9565cba7de84b2951cd42ccb555ce98bdade96ab
Júnior Medeiros <98979459+Medeiros000@users.noreply.github.com>
2024-07-19 03:38:32 -0300

    Update h-functions.php

--- helpers/h-functions.php
+++ helpers/h-functions.php
@@ -20,7 +20,7 @@ function h_head(string $title, string $content = null)
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="../css/style.css">
         ' . $content . '         
         <title>' . $title . '</title>
       </head>
